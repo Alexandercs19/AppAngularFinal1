247fc0796cbcf8d84287fbb1c361a8a43b940837
Alexandercs19 <deivialexander6@gmail.com>
2022-12-13 17:38:13 -0400

    Parte final Arreglo

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit, ViewEncapsulation   } from '@angular/core';
-import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, Zoom, EffectCoverflow} from 'swiper';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import SwiperCore, {
+  Autoplay,
+  Keyboard,
+  Pagination,
+  Scrollbar,
+  Zoom,
+  EffectCoverflow,
+} from 'swiper';
 
-SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom, EffectCoverflow]);
+SwiperCore.use([
+  Autoplay,
+  Keyboard,
+  Pagination,
+  Scrollbar,
+  Zoom,
+  EffectCoverflow,
+]);
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class InicioPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
