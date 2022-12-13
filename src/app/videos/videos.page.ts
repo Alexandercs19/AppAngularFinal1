84d0891ba945eaab40a7401dd5758f4a1c9e6399
Alexandercs19 <deivialexander6@gmail.com>
2022-12-13 01:25:51 -0400

    Parte45
    Yo estoy bien harto de Hector ese mmñ
    
    Co-Authored-By: Yuneno <111202711+Yuneno@users.noreply.github.com>

--- src/app/videos/videos.page.ts
+++ src/app/videos/videos.page.ts
@@ -6,21 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./videos.page.scss'],
 })
 export class VideosPage implements OnInit {
-  optdata: any[] = [];
-  constructor() {
-    this.Obtener();
-  }
-  Obtener = async () => {
-    await fetch('https://adamix.net/defensa_civil/def/videos.php')
-      .then((response) => response.json())
-      .then((data) => {
-        // ASIGNANDO LOS DATOS DE LA API A MI ARRAY.
-        console.log(data.datos);
-        this.optdata = data.datos;
-      });
-  };
+  constructor() {}
   ngOnInit() {}
-  ionViewDidLoad() {
-    this.Obtener();
-  }
+
+  // optdata: any[] = [];
+  // constructor() {
+  //   this.Obtener();
+  // }
+  // Obtener = async () => {
+  //   await fetch('https://adamix.net/defensa_civil/def/videos.php')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // ASIGNANDO LOS DATOS DE LA API A MI ARRAY.
+  //       console.log(data.datos);
+  //       this.optdata = data.datos;
+  //     });
+  // };
+  // ngOnInit() {}
+  // ionViewDidLoad() {
+  //   this.Obtener();
+  // }
 }
